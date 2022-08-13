886c6b49afcf04ffd52fd3b4005ac6f78b51653b
Brian Gawalt <bgawalt@gmail.com>
2022-08-13 13:13:38 -0700

    Handle NEW command.

--- tweet_mine.py
+++ tweet_mine.py
@@ -70,7 +70,8 @@ This table is called 'game_state' with columns:
 
 *  'move_id', INTEGER: A zero-indexed identifier for moves made so far.
 *  'command', TEXT: Either 'new', 'dig' or 'flag'.
-*  'gridpoint', TEXT: The same format as the above command line flag.
+*  'gridpoint_row', INTEGER: The row in which to dig/flag.
+*  'gridpoint_col', INTEGER: The column in which to dig/flag.
 *  'tweet_id', TEXT: The tweet ID string for the tweet this move generated.
 
 Inserts to this table will come immediately following each call to Twitter's
@@ -151,8 +152,10 @@ from __future__ import annotations
 import dataclasses
 import enum
 import json
+import minesweeper
 import requests
 import requests_oauthlib
+import sqlite3
 import sys
 
 from collections.abc import Sequence
@@ -160,6 +163,22 @@ from collections.abc import Sequence
 
 # Where to send tweets:
 POST_TWEET_URL = 'https://api.twitter.com/1.1/statuses/update.json'
+
+_CREATE_TABLE_QUERY = """
+    CREATE TABLE game_state (
+        move_id integer,
+        command text,
+        gridpoint_row integer,
+        gridpoint_col integer,
+        tweet_id text
+    )
+"""
+
+_UPDATE_STATE_QUERY = """
+    INSERT INTO game_state (
+        move_id, command, gridpoint_row, gridpoint_col, tweet_id)
+    VALUES (?, ?, ?, ?, ?)
+"""
 
 
 @enum.unique
@@ -232,6 +251,27 @@ def oauth_from_config_file(config_filename: str) -> requests_oauthlib.OAuth1:
         resource_owner_secret=config['access_secret']
     )
 
+def initialize_game_state(db_cursor: sqlite3.Cursor) -> None:
+    """Creates the game state table, or dies trying."""
+    try:
+        db_cursor.execute(_CREATE_TABLE_QUERY)
+    except sqlite3.OperationalError as op_err:
+        raise RuntimeError(
+        "Seems like you tried initializing a game via 'NEW' on a DB file"
+        "that already has a game going.") from op_err
+
+def update_game_state(db_cursor: sqlite3.Cursor,
+                      move_id: int,
+                      command: Command,
+                      gridpoint_row: int,
+                      gridpoint_col: int,
+                      tweet_id: str) -> None:
+    """Updates the game state table with the latest move."""
+    db_cursor.execute(
+        _UPDATE_STATE_QUERY,
+        (move_id, command.name, gridpoint_row, gridpoint_col, tweet_id)
+    )
+
 
 def main():
     flags = CommandLineFlags.from_argv(sys.argv)
@@ -246,6 +286,28 @@ def main():
                     'auto_populate_reply_metadata': True}
     req = requests.post(url=POST_TWEET_URL, data=request_data, auth=oauth)
 
+    db_conn = sqlite3.connect(flags.sqlite_filename)
+    db_cursor = db_conn.cursor()
+    seed = hash('A super secret salt; no peeking ' + flags.sqlite_filename)
+    ms_game = minesweeper.MinesweeperGame.Beginner(seed)
+
+    if flags.command is Command.NEW:
+        move_id = 0  # First move
+        initialize_game_state(db_cursor)
+        db_conn.commit()
+    else:
+        # TODO:
+        #   - load old moves
+        #   - apply old moves
+        #   - apply new move
+        raise NotImplementedError('All things in time')
+
+    # TODO: stop printing. start tweeting!
+    ms_game.Print(include_ticks=True)
+    update_game_state(db_cursor, move_id, flags.command, flags.gridpoint_row,
+                      flags.gridpoint_col, 'NO TWEETS YET!!')
+    db_conn.commit()
+    db_conn.close()
 
 
 if __name__ == "__main__":
